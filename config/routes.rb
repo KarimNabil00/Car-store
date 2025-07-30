499a4e5073aacd1087cd564cb9f439d86d9d7043
Karim Nabil <karimnabil290l@gmail.com>
2025-07-30 15:10:04 +0300

    adding prdoucts scaffold to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
   get "about" => "about#index"
   get "home" => "home#index"
   get "cars" => "cars#index"
-  get "sign_up" => "registrations#new"
-  post "users" => "registrations#create"
+  #get "sign_up" => "registrations#new"
+  #post "users" => "registrations#create"
   root "home#index"
+ resources :users, only: [:create], controller: "registrations"
+ get 'sign_up', to: 'registrations#new', as: :sign_up
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_28_101416) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_30_120050) do
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
