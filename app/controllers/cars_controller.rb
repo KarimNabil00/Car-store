b1130873be3353434d3af968c639415acd8ca419
Karim Nabil <karimnabil290l@gmail.com>
2025-08-11 14:47:38 +0300

    some updates in the index and the car page

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,7 +3,7 @@ class CarsController < ApplicationController
 
   # GET /cars or /cars.json
   def index
-    @cars = Car.all
+    @cars = Car.where(available: true)
   end
 
   # GET /cars/1 or /cars/1.json
